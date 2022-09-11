3e17f11da0058aed31fc817ed9fb4faadc5946dd
tadanokaida <kevinkhuang2002@gmail.com>
2022-09-11 19:50:39 -0400

    draft 2

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -176,10 +176,8 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
-        for (int i = 0; i <= arr.length; i++) {
-            if (i % 2 == 0) {
+        for (int i = 1; i <= arr.length - 1; i += 2) {
                 current_sum += arr[i];
-            }
         }
         return current_sum;
     }
